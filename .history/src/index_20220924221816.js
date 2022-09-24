07da47a348fbff0b8748a75fbe5c654f5b67f6be
EgorSherstnev <svarog91@list.ru>
2022-09-25 00:41:05 +0300

    add Latitude

--- .history/src/index_20220924221816.js
+++ .history/src/index_20220924221816.js
@@ -11,23 +11,13 @@ class App extends React.Component {
             (position) => {
                 this.setState({lat: position.coords.latitude})
             },
-            (err) => {
-                this.setState({errorMessage: err.message })
-            }
+            (err) => console.log(err)
         );
     }
 
 
     render () {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <div>latitude: {this.state.lat}</div>
-        }
-
-        return <div>Loading!</div>
+        return <div>Latitude: {this.state.lat}</div>
     }
 }
 
